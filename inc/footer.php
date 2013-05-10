0a5997489bb49c0db1777aa37cf4eae7d5d28f7d
Cameron Pierson <cjaypierson@gmail.com>
2013-05-10 05:40:18 -0400

    working on footer again

--- inc/footer.php
+++ inc/footer.php
@@ -6,9 +6,7 @@
 			<div class="account">
 				
 				<?php 
-					if (session_id() == '') {
-						session_start();
-					}
+					session_start();
 					if (isset($_SESSION['user'])) { ?>
 			        	<a href="logout.php" onclick="return confirm('Are you sure you want to logout?')">Logout</a>
 			    <?php } else { ?>
