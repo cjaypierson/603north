314b1d040053ec2802154b8afca8cae445acbd4b
Cameron Pierson <cjaypierson@gmail.com>
2013-05-10 05:55:59 -0400

    working on footer again

--- inc/footer.php
+++ inc/footer.php
@@ -5,19 +5,13 @@
 			?>
 			<div class="account">
 				
-				<?php
-					if (!$db_current) { 
-						session_start();
-						db_link = mysql_connect('localhost', 'pierson3_cameron', 'Summer9(');
-						$db_current = mysql_select_db('pierson3_603north', $db_link);	
-					}
-					$query = 'SELECT * FROM users WHERE session_id = "' . session_id() . '" LIMIT 1';
-					$userResult = mysql_query($query);
-					if (mysql_num_rows($userResult) == 1){ ?>
+				<?php 
+					//session_start();
+					//if (isset($_SESSION['user'])) { ?>
 			        	<a href="logout.php" onclick="return confirm('Are you sure you want to logout?')">Logout</a>
-			    <?php } else { ?>
+			    <?php //} else { ?>
 			    			<a href="login.php">Login</a>
-			    <?php } ?>
+			    <?php //} ?>
 
 			</div>
 
