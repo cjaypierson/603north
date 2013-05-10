1a6aa5b3a78a45797a2bb133c368f395d1afdaee
Cameron Pierson <cjaypierson@gmail.com>
2013-05-09 20:44:35 -0400

    testing hook, added mailto link

--- availability.php
+++ availability.php
@@ -4,7 +4,7 @@
 ?>
 
 
-<h2>For our latest availability please contact us at info@603north.com</h2>
+<h2>For our latest availability please contact us at <a href="mailto:info@603north.com?Subject=Wedding%20Cinematography">info@603north.com</a></h2>
 
 
 
