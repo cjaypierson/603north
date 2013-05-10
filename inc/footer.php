da7f7428e98a8491b0595c1a9766c6513d143eec
Cameron Pierson <cjaypierson@gmail.com>
2013-05-10 05:58:12 -0400

    working on footer again

--- inc/footer.php
+++ inc/footer.php
@@ -10,7 +10,7 @@
 					//if (isset($_SESSION['user'])) { ?>
 			        	<a href="logout.php" onclick="return confirm('Are you sure you want to logout?')">Logout</a>
 			    <?php //} else { ?>
-			    			<a href="login.php">Login</a>
+			    			<a href="login.php">Login</a>     
 			    <?php //} ?>
 
 			</div>
