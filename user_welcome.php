9477ee4f3c33e66291fdd77ea57554d5d2083b86
Cameron Pierson <cjaypierson@gmail.com>
2013-05-11 03:12:30 -0400

    added email name to welcome page

--- user_welcome.php
+++ user_welcome.php
@@ -6,6 +6,8 @@ include(ROOT_PATH . 'inc/header.php');
 
 $query = 'SELECT * FROM users WHERE session_id = ' . session_id() . '" LIMIT 1';
 $email_result = mysql_query($query);
+echo $email_result;
+print_r($email_result);
 
 echo '<h2>Welcome to 603north ' . $email_result['email'] . '</h2>'; 
 
