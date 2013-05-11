cc2da29aa6053e74a4818e88e3e1ee699e649552
cjaypierson <cjaypierson@gmail.com>
2013-05-10 20:32:58 -0400

    Checking session info

--- inc/footer.php
+++ inc/footer.php
@@ -5,8 +5,9 @@
 			?>
 			<div class="account">
 				
-				<?php 
-					//session_start();
+				<?php
+					session_start();
+					print_r($_SESSION);
 					//if (isset($_SESSION['user'])) { ?>
 			        	<a href="logout.php" onclick="return confirm('Are you sure you want to logout?')">Logout</a>     
 			    <?php //} else { ?>
